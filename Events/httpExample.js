7dd6b6e877fcf86981d036a721f3c2618c8c79d2
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-04-19 00:20:01 +0400

    adding a comment

--- Events/httpExample.js
+++ Events/httpExample.js
@@ -10,6 +10,7 @@ const serverNotGood = http.createServer((req, res) => {
 })
 
 //this callback function will be invoked every time someone visits our server
+//we listen for request event and when that request comes in server emits the request event and then we can listen/respond for it
 server.on('request', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello World\n')
