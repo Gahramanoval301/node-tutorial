0fa0add9c9bdada786f5055ca060321833dad1e5
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-04-20 17:19:36 +0400

    small http example

---
+++ Streams/httpExample.js
@@ -0,0 +1,13 @@
+//practical example when streams come in handy
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    const fileStream = fs.createReadStream('../content/big.txt', { encoding: 'utf8' })
+    fileStream.on('open', () => {
+        fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+        res.end(err)
+    })
+})
